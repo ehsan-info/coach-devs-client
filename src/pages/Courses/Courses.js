25aaa0173967422a0defaf96f1503b86ab210af1
ehsan-info <ehsanurrahman1992@gmail.com>
2022-10-27 11:25:34 +0300

    pdf print added

--- src/pages/Courses/Courses.js
+++ src/pages/Courses/Courses.js
@@ -8,10 +8,9 @@ const Courses = () => {
     console.log(allCourses);
     return (
         <div>
-            <h2>This is Courses cat page {allCourses.length}</h2>
+            <h2>Try out our courses {allCourses.length}</h2>
             <Container>
                 <Row>
-
                     {
                         allCourses.map(course => <Col lg='6'><FeaturedCourse key={course._id} course={course}></FeaturedCourse></Col>)
                     }
--- src/shared/FeaturedCourse/FeaturedCourse.js
+++ src/shared/FeaturedCourse/FeaturedCourse.js
@@ -1,23 +1,39 @@
-import React from 'react';
-import Carousel from 'react-bootstrap/Carousel';
-import image1 from '../../assets/carousal/1.jpg';
-import image2 from '../../assets/carousal/2.jpg';
-import image3 from '../../assets/carousal/3.jpg';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
-import Image from 'react-bootstrap/Image';
-import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import { FaEye, FaStar } from 'react-icons/fa';
+import { FaDownload, FaEye, FaStar } from 'react-icons/fa';
+import { render } from 'react-dom';
+import { useReactToPrint } from 'react-to-print';
+import { Button } from 'react-bootstrap';
+
 const FeaturedCourse = ({ course }) => {
     console.log(course);
-    const { _id, category_id, course_name, thumbnail_url, total_view, image_url, rating, trainer_info, details, price } = course;
+    const { _id, category_id, course_name, total_view, image_url, rating, trainer_info, details, price } = course;
+    const componentRef = useRef();
+    const handlePrint = useReactToPrint({
+        content: () => componentRef.current,
+    });
     return (
         <div>
+
             {
-                <Card className="mt-3">
+                <Card className="mt-3" ref={componentRef}>
+                    <Card.Header className='d-flex justify-content-end'>
+                        <div></div>
+                        <div>
+
+                            <Button className='bg-light btn-outline-secondary' onClick={handlePrint}><FaDownload className='text-dark'></FaDownload></Button>
+                        </div>
+                    </Card.Header>
                     <Card.Body>
                         <Card.Img style={{ height: '150px' }} variant="top" src={image_url} />
                         <Card.Title style={{ height: '100px' }}>{course_name}</Card.Title>
+                        <Card.Text>
+                            <small><span>Instructor: </span>{trainer_info?.name}</small>
+                        </Card.Text>
+                        <Card.Text>
+                            <small><span>Position: </span>{trainer_info?.position}</small>
+                        </Card.Text>
                         <Card.Text>
                             {
                                 details.length > 200 ?
